16b827faa20453c57ef5e0406b505ec53f753054
Maria-Luisa1599 <maria.luisa.bcramos2008@gmail.com>
2025-08-26 20:00:06 -0300

    Banco de dados atualizações

---
+++ bd/alertaBd.js
@@ -0,0 +1,4 @@
+const hoje = new Date();
+const dataFormatada = hoje.toLocaleDateString("pt-BR"); 
+
+console.log(dataFormatada); 
--- bd/clienteBd.js
+++ bd/clienteBd.js
@@ -3,7 +3,7 @@ async function adicionarCliente() {
   const senhaCliente = document.getElementById('senhaCliente').value;
 
   const { data, error } = await supabase
-    .from('cliente')
+    .from('usuario')
     .insert([{ nome: nomeCliente, senha: senhaCliente }]);
 
   if (error) {
@@ -12,5 +12,48 @@ async function adicionarCliente() {
     console.log("Cliente cadastrado:", data);
     document.getElementById('nomeCliente').value = "";
     document.getElementById('senhaCliente').value = "";
+    listarClientes()
+  }
+}
+
+async function listarClientes() {
+  const { data, error } = await supabase
+    .from('usuario')
+    .select('*');
+
+  if (error) {
+    console.error("Erro ao mostrar:", error);
+  } else {
+    console.log(data);
+  }
+
+}
+
+async function verificarCliente() {
+  const nomeCliente = document.getElementById('nomeClienteLogin').value;
+  const senhaCliente = document.getElementById('senhaClienteLogin').value;
+
+
+  if (!nomeCliente || !senhaCliente) {
+    console.log('Todos os campos precisam ser preenchidos')
+  } else {
+    const { data, error } = await supabase
+      .from('usuario')
+      .select('*')
+      .eq('nome', nomeCliente);
+
+    if (error) {
+      console.error("Erro ao mostrar:", error);
+    } else {
+      if (data.length <= 0) {
+        console.log("Cliente não cadastrado!");
+      } else if (data[0].senha == senhaCliente) {
+        console.log("Login efetuado com sucesso!");
+        localStorage.setItem("usuarioLogado", data[0].nome);
+        window.location.replace('../fluxocash.html');
+      } else {
+        console.log("Senha errada!");
+      }
+    }
   }
 }
