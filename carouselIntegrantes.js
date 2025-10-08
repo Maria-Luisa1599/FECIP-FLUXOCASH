0b06a94ec996383cf5f503c335d0d18b85a38758
Maria-Luisa1599 <maria.luisa.bcramos2008@gmail.com>
2025-10-07 21:08:58 -0300

    carrousel

--- carouselIntegrantes.js
+++ carouselIntegrantes.js
@@ -2,8 +2,10 @@ function direcao(e){
     var direcao = document.getElementById("contentSlide")
 
 if(e == 1){
+    //se apertar o botao pra esquerda, o carousel de move -200
     direcao.scrollLeft = direcao.scrollLeft - 200;
 }else if(e == 2){
+    //se apertar o botao pra direita, o carousel de move +200
     direcao.scrollLeft = direcao.scrollLeft + 200;
 
 }
